3ac481598db63166f9d52ffbf8f4734bdb9c27b8
And4Web <anands2807@gmail.com>
2022-05-27 21:09:45 +0530

    data fetching done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
 
 function App() {
+  const url = "https://course-api.com/react-tabs-project";
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setLoading(false);
+    setJobs(newJobs);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  console.log(jobs);
+
   return (
     <div className="App">
       <h1>hello</h1>
