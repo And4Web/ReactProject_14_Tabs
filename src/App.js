17824f5bf6b438de55c4ccf5642c4083371d0ed7
And4Web <anands2807@gmail.com>
2022-05-27 22:28:15 +0530

    jobs page created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-
+import { FaAngleDoubleRight } from "react-icons/fa";
 import "./App.css";
 
 function App() {
   const url = "https://course-api.com/react-tabs-project";
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
     const response = await fetch(url);
@@ -21,9 +22,40 @@ function App() {
 
   console.log(jobs);
 
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  const valueSetter = (num) => {};
+
+  const { id, title, dates, duties, company } = jobs[value];
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      <h1>New Jobs available</h1>
+      {jobs.map((job, index) => {
+        return (
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <button key={job.id}>{job.company}</button>
+          </div>
+        );
+      })}
+
+      <div>
+        <h2>{title}</h2>
+        <h3>{company}</h3>
+        <h4>{dates}</h4>
+        {duties.map((duty) => {
+          return (
+            <article style={{ display: "flex", alignItems: "center" }}>
+              <FaAngleDoubleRight />
+              <p>{duty}</p>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
